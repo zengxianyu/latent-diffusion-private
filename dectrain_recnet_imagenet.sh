3241341fa9f068b3a8c9e0485d83ae05cd24eea8
yzeng22 <zengxianyu18@gmail.com>
2023-01-16 13:20:53 -0500

    imagenerec

---
+++ dectrain_recnet_imagenet.sh
@@ -0,0 +1,16 @@
+python main.py --base configs/autoencoder/autoencoder_vq_8x8x3.yaml -t --gpus 0,1 \
+	model.target=ldm.models.autoencoder.VQDecModel \
+	model.params.image_key="JPEG" \
+	data.params.train.target=ldm.data.listdata.SimpleListDataset \
+	data.params.train.params.data_dir=/data/common/ILSVRC/Data/CLS-LOC/train \
+	data.params.train.params.data_list=/data/yzeng22/imagenet_train.txt \
+	data.params.train.params.postfix=["JPEG"] \
+	data.params.train.params.augpf=["JPEG"] \
+	data.params.validation.target=ldm.data.listdata.SimpleListDataset \
+	data.params.validation.params.data_dir=/data/common/ILSVRC/Data/CLS-LOC/val \
+	data.params.validation.params.data_list=/data/yzeng22/imagenet_val.txt \
+	data.params.validation.params.postfix=["JPEG"] \
+	data.params.validation.params.augpf=["JPEG"] \
+	data.params.batch_size=16 \
+	--finetune vq-f4.ckpt \
+	--name vqgan-imagenetinit-nocls \
